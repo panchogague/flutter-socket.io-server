f12e55ed86cbf9b2c853e98e6b23a471c0142382
Paco.Gonzalez <Paco.Gonzalez@impartx.com>
2022-08-24 11:58:32 +0100

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,36 @@
 const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Iron Maiden"));
+bands.addBand(new Band("Bad Bunny"));
+bands.addBand(new Band("The Cure"));
 
 io.on("connection", (client) => {
   console.log("client connected");
+
+  client.emit("active-bands", bands.getBands());
 
   client.on("disconnect", () => {
     console.log("client disconnected");
   });
 
-  client.on("message", (payload) => {
-    console.log(payload);
-    io.emit("message", { admin: "New message" });
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("add-band", (payload) => {
+    const newBand = new Band(payload.name);
+    bands.addBand(newBand);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("delete-band", (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
   });
 });
